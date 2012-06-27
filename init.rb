99a139dd0860f4c13cb49bcd28baa667e6f86d5d
MAEDA Go <maeda@farend.jp>
2012-06-27 16:32:40 +0900

    blank plugin.

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_gantt_with_date do
+  name 'Redmine Gantt With Date plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
